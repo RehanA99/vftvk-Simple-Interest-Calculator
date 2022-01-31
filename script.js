b2951630d090b1d08f48205f48bc113495c2fa56
RehanA99 <rehanearn99@gmail.com>
2022-01-31 19:11:40 +0530

    index.html, script.js and style.css have been updated.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+	var principal = document.getElementById("principal").value ;
+    	var rate = document.getElementById("rate").value ;
+	var years = document.getElementById("years").value ;
+	var interest = principal * years * rate / 100 ;
+
+	var year = new Date().getFullYear() + parseInt(years) ;
+//alert( "Principal : " + principal + "; Rate : " + rate + "; No. of Years : " + years + "; Interest : " + interest ) ;
+
+	if( principal <= 0 ) {
+		alert("Enter a positive number") ;
+		principal.focus() ;
+		return false ;
+	} else {
+		document.getElementById("result").innerHTML = "If you deposite \<mark>" + principal + "\</mark>,\<br\>at an interest rate of \<mark>" + rate + "%\</mark>.\<br\>You will receive an amount of \<mark>" + interest + "\</mark>,\<br\>in the year \<mark>" + year + "\</mark>" ;
+	}
+	return true ;
 }
-        
+
+function updateRate() {
+	var rateval = document.getElementById("rate").value ;
+	document.getElementById("rate_val").innerText = rateval ;
+ }       
